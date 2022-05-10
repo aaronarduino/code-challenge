688b7dc767949c4d128198b0720dcdb48535ae5b
Aaron Alexander <aarona.arduino@gmail.com>
2022-05-09 22:05:04 -0500

    base case solved

---
+++ code-challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace code_challenge
+{
+    class Program
+    {
+        const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        static void Main(string[] args)
+        {
+            Dictionary<char, bool> d = new Dictionary<char, bool>();
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Please enter chars:");
+            string userInput = Console.ReadLine().ToUpper();
+
+            string result = String.Concat(Alphabet.Where(c => !userInput.Contains(c)));
+
+            Console.WriteLine();
+            Console.WriteLine("The result is:");
+            Console.WriteLine(result);
+        }
+    }
+}
