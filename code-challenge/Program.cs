da59a0a503b915265b845e9fd9124c406fabcf9a
Aaron Alexander <aarona.arduino@gmail.com>
2022-05-11 00:48:49 -0500

    added check for 256 chars

--- code-challenge/Program.cs
+++ code-challenge/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace code_challenge
@@ -7,13 +8,34 @@ namespace code_challenge
     class Program
     {
         const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const int ReadBufferSize = 256;
 
         static void Main(string[] args)
         {
+            bool verified = false;
+            string userInput = "";
+
+            // 1. Get user input
             Console.WriteLine("Hello!");
-            Console.WriteLine();
-            Console.WriteLine("Please enter chars:");
-            string userInput = Console.ReadLine().ToUpper();
+            while (!verified)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter chars:");
+                userInput = Console.ReadLine().ToUpper();
+
+                if(userInput.Length <= ReadBufferSize)
+                {
+                    verified = true;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid input. Input is limited to 256 chars.");
+                }
+            }
+
+            // 2. Print out alphabet minus any chars from the user input
+            //    and count of chars left in alphabet
             string result = String.Concat(Alphabet.Where(c => !userInput.Contains(c)));
             Console.WriteLine();
             Console.WriteLine("The result is:");
